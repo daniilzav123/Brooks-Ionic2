b623f51ec0addb1608b62b6ebdb5a90ed346eb37
Daniil Zavyalov <daniilzavup@gmail.com>
2017-08-14 06:18:22 +0200

    Added Intro Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { IntroScreen } from '../pages/IntroScreen/IntroScreen';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any = IntroScreen;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ src/pages/IntroScreen/IntroScreen.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'introscreen-home',
+  templateUrl: 'home.html'
+})
+export class IntroScreen {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
